fbf3c1a6f78eba5935c9898495fe58faa705e1ce
DESKTOP-73MDNV8\ez220406 <moonhj0105@naver.com>
2022-06-09 21:52:04 +0900

    0609_update

--- Training_day2_3/src/change.java
+++ Training_day2_3/src/change.java
@@ -1,26 +1,42 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class change {
 
-	
+
+
 		  public static void main(String[] args) {
 		        Scanner sc = new Scanner(System.in);
-		        int i, temp;
+		        int a;
+		        ArrayList<Integer> list = new ArrayList<>();
 		        System.out.print("수 입력 : ");
-		        i = sc.nextInt();
-		        System.out.print("2진수 :");
+		        a = sc.nextInt();
+		        list.add(1);
+		        
+		        for(int i=0; i<list.size(); i++) {
+		        	list.add(a%2);
+		        	a=a/2;      	
+		        }
+		        
+		        for (int i=list.size(); i>=0; i--) {
+		        	System.out.print(list.get(i));
+		        }
+/**        System.out.print("2진수 :");
 		        while(true) {
 		           //입력한 값의 나머지를 temp에 넣어 나머지를 구한다.
-		           temp=i%2;
-		           //입력한 값의 몫을 i에 넣어준다.
-		           i=i/2;
+		           temp=a%2;
+
+		           a=a/2;
 		            
 		            System.out.print(temp);
 		            //몫 i의 값이 0이 될때 까지 반복한다.
-		            if(i==0) {
-		                break;
-		            }
+		            if(a==0) {
+		                break; 
+		            } **/
 		            
-		        }
+		        
 		    }
-		}
+
+
+}
+		
 	
--- Training_day2_3/src/shift.java
+++ Training_day2_3/src/shift.java
@@ -7,28 +7,29 @@ public class shift {
 				
 			System.out.println("숫자를 입력하세요");
 			int a = stdIn.nextInt();
-				
+			
 			System.out.println("시프트 방향을 고르세요 1.왼쪽 2. 오른쪽");
-			int b = stdIn.nextInt();
-			
-			int c = 2;
 				
-			
-				
-			 while(c != 1) {
-				
+			while(true) {
+
+				System.out.println("시프트 방향을 고르세요 1.왼쪽 2. 오른쪽");
+				int b = stdIn.nextInt();
 				if(b==1) {
-				
-					System.out.println("왼쪽으로 이동:" + a*2);
+				System.out.println("왼쪽으로 이동:" + a*2);
+				a=a*2;
 				}
 				
 				else if(b==2) {
-					System.out.println("오른쪽으로 이동:" + a/2);
-				
+				System.out.println("오른쪽으로 이동:" + a/2);
+				a=a/2;
 				}
 				
 				System.out.println("그만할까요? 1..No 2..Yes");
-				 c = stdIn.nextInt();
+				 int c = stdIn.nextInt();
+				 if(c==2) {
+					 break;
+				 }
+					 
 		
 			 	}
 
